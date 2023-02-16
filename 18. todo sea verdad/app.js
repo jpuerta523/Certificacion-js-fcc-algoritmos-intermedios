31acb6dd3bbe35121a985d07b12aeb6d335bdd0a
Pablo Puerta <jpuerta523@gmail.com>
2023-02-16 09:52:44 -0500

    todo sea verdad aprobado FCC

---
+++ 18. todo sea verdad/app.js
@@ -0,0 +1,10 @@
+
+function truthCheck(collection, pre) {
+    return collection
+            .map(x => x[pre])
+            .every(a => Boolean(a))
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"))
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"))
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role"))
